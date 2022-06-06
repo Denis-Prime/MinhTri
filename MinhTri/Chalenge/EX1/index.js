8c4512416f56fa1b68ddb8ceef20322b159fdfdf
Denis-Prime <deniscode123@gmail.com>
2022-06-06 23:01:51 +0700

    add

--- MinhTri/Chalenge/EX1/index.js
+++ MinhTri/Chalenge/EX1/index.js
@@ -1,19 +1,17 @@
-const Ele = document.getElementsByTagName("input")
-Ele[0].value = "input a"
-Ele[1].value = "input b"
+const inp1 = document.getElementById("inp1")
+const inp2 = document.getElementById("inp2")
+const button = document.getElementById("button")
+const res = document.getElementById("res")
 
-document.getElementsByClassName('btn')[0].addEventListener("click",()=>{
-    const valuea = parseInt( Ele[0].value)
-    const valueb = parseInt(Ele[1].value)
-    i = valuea<valueb? valuea:valueb
-    j = valuea>valueb? valuea:valueb 
-    for(a = i;a<=j;a++){
-        for( g = 2;g<a;g++){
-            if(a%g ==0) break
-            if(a%g !=0 && g==a-1){
-                document.getElementsByClassName("result")[0].innerText+=" "+g
-                console.log(g);
-            }
+button.addEventListener("click",()=>{
+    const minvalue = parseInt(inp1.value)<parseInt(inp2.value)? parseInt(inp1.value):parseInt(inp2.value)
+    const maxvalue = parseInt(inp1.value)>parseInt(inp2.value)? parseInt(inp1.value):parseInt(inp2.value)
+
+    for(let i = minvalue;i<maxvalue;i++){
+        if(Math.sqrt(i) - parseInt(Math.sqrt(i)) == 0 && i>=0){
+            res.innerText +=i+"  ;  "
         }
     }
 })
+
+
--- MinhTri/Chalenge/EX2/index.js
+++ MinhTri/Chalenge/EX2/index.js
@@ -1,10 +1,37 @@
-function numberOneTriangle(para) {
-    let str = ""
-    for(i = 0;i<para;i++){
-        str = str+="*"
-        console.log(str)
-    }
-}
-numberOneTriangle(5)
-numberOneTriangle(10)
-numberOneTriangle(25)
+const container = document.getElementById("container")
+const span = document.getElementById("span")
+const span2 = document.getElementById("span2")
+const btnChange = document.getElementById("btn-change")
+const btnCopy = document.getElementById("btn-copy")
+const btncopy2 = document.getElementById("btn-copy2")
+
+btnChange.addEventListener("click",()=>{
+    let red = Math.floor(Math.random()*255)
+    let green = Math.floor(Math.random()*255)
+    let yelow = Math.floor(Math.random()*255)
+    let redhex = red.toString(16)
+    let greenhex = green.toString(16)
+    let yelowhex = yelow.toString(16)
+    let strGrb = "grb("
+    strGrb+=red+","+green+","+yelow+")"
+    span2.innerText = strGrb
+    let strHex = "#"
+    strHex+= redhex+greenhex+yelowhex
+    span.innerText = strHex
+    container.style.background = strHex
+})
+btnCopy.addEventListener("click",()=>{
+    const cb = navigator.clipboard;
+    const para = document.querySelector("#span");
+    cb.writeText(para.innerText).then(() => alert('color copiled'));
+})
+
+btncopy2.addEventListener("click",()=>{
+    const cb = navigator.clipboard;
+    const para = document.querySelector("#span2");
+    cb.writeText(para.innerText).then(() => alert('color copiled'));
+})
+
+
+
+
